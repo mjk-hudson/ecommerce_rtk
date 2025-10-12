070544bd4a33ccf88f45da02004e8e825a869cd6
mjk-hudson <majik.hudson@icloud.com>
2025-10-12 17:04:54 -0400

    Finished Application

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import ShoppingCart from './Components/ShoppingCart';
 import './App.css'
 const App = () => {
   return (
- 
+    <>
       <div>
         <h1 className='app-heading'>E-Commerce Application</h1>
         <ProductList />
         <ShoppingCart />
       </div>
-
+    </>
   );
 };
 
